f5eeff5d7bc587eb344d11323ca9844f5f1d8ce7
Bilal <bilalmirza79500@gmil.com>
2021-11-29 14:45:01 +0500

    navigation

---
+++ src/Screen3.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import {
+    Button,
+    StyleSheet,
+    Linking,
+    Text,
+    View,
+    ScrollView,
+    RefreshControl,
+    FlatList,
+    SectionList,
+    TextInput,
+    TouchableOpacity,
+    Pressable,
+    Alert,
+    ToastAndroid,
+    Modal,
+    Image,
+    ImageBackground,
+    navigation,
+
+} from 'react-native';
+
+
+
+import { useNavigation } from '@react-navigation/core';
+
+
+export default function Screen3({}) {
+    const Navigation = useNavigation();
+    // const onPressHandler = () => {
+        // navigation.navigate('Screen_A');
+        // navigation.goBack();
+        
+    // }
+
+    return (
+        <View style={style.body}>
+            <Text style={style.text}>
+                Screen 3
+            </Text>
+            <Pressable
+                onPress={()=>{Navigation.navigate('Screen_1')}}
+                style={({ pressed }) => ({ backgroundColor: pressed ? 'red' : '#0f0' })}
+            >
+                <Text style={style.text}>
+                    Back To Screen 1
+                </Text>
+            </Pressable>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    body: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    text: {
+      fontSize: 40,
+      fontWeight: 'bold',
+      margin: 10,
+    },
+  })
